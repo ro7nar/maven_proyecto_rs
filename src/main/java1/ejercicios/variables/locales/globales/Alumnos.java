e8c5e6b4b98413bf3bfdef37e7d1fec46fec5714
Roynar Sando <roynar-sandito@hotmail.com>
2021-09-06 20:11:39 -0500

    Taller

--- src/main/java1/ejercicios/variables/locales/globales/Alumnos.java
+++ src/main/java1/ejercicios/variables/locales/globales/Alumnos.java
@@ -2,4 +2,19 @@ package ejercicios.variables.locales.globales;
 
 public class Alumnos {
 
+	private String nombre;
+	private String apellido;
+	private int cedula;
+	
+	
+	
+	public void datosEstudiante() {
+		this.nombre = "Jorge";
+		this.apellido = "Salome";
+		this.cedula = 1500955234;
+		String Colegio = "Alvaro Balladares";
+		String Clase = "A1";
+		
+		
+	}
 }
