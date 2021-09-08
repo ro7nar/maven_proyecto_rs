291ed2803be41d03b46fb71600b1ed3df5c4e7ef
Roynar Sando <roynar-sandito@hotmail.com>
2021-09-07 19:59:47 -0500

    Taller en clases

--- src/main/java1/ejercicio/clase/string/Main.java
+++ src/main/java1/ejercicio/clase/string/Main.java
@@ -7,22 +7,25 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 1 contains
+		
+		
+		
 		Scanner lector = new Scanner(System.in);
-		//String Frase = "Programacion metodo II";
-		//Scanner lector = new Scanner(System.in);
-		//System.out.println("Ingrese el nombre del libro");
-		//String palabras = lector.nextLine();
+		String Frase = "Programacion metodo II";
+		
+		System.out.println("Ingrese el nombre del libro");
+		String palabras = lector.nextLine();
 
-		//boolean resultado = Frase.contains(palabras);
-		//System.out.println("Contiene la palabra"+resultado);
+		boolean resultado = Frase.contains(palabras);
+		System.out.println("Contiene la palabra"+resultado);
 // 2 equals
 		
-		//System.out.println("**************************");
-		//String cadena1= "Carlos";
-		//System.out.println("Ingrese la segunda cadena a comparar");
-		//String cadena2= lector.nextLine();
-		//boolean resultado2 = cadena1.equals(cadena2);
-		//System.out.println("las cadenas son iguales: "+resultado2);
+		System.out.println("**************************");
+		String cadena1= "Carlos";
+		System.out.println("Ingrese la segunda cadena a comparar");
+		String cadena2= lector.nextLine();
+		boolean resultado2 = cadena1.equals(cadena2);
+		System.out.println("las cadenas son iguales: "+resultado2);
 		
 		// 3. y 4. Upper Lower
 		System.out.println("*********************");
--- src/main/java1/ejercicios/variables/locales/globales/Alumnos.java
+++ src/main/java1/ejercicios/variables/locales/globales/Alumnos.java
@@ -2,12 +2,24 @@ package ejercicios.variables.locales.globales;
 
 public class Alumnos {
 
-	private String nombre;
+	private String nombre = "Jaramillo";
+			
 	private String apellido;
 	private int cedula;
+	public static String pais = "Ecuador";
+	public static String provincia = "Pichincha";
 	
 	
-	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
 	public void datosEstudiante() {
 		this.nombre = "Jorge";
 		this.apellido = "Salome";
