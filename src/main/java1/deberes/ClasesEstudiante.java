8da7b6c8828788e0c6aed818315c6a10ddbb9e62
Roynar Sando <roynar-sandito@hotmail.com>
2021-09-13 16:28:43 -0500

    Deber final

---
+++ src/main/java1/deberes/ClasesEstudiante.java
@@ -0,0 +1,13 @@
+package deberes;
+
+// No se puede extender una clase final Padre que sea final
+public class ClasesEstudiante extends DeberFinal {
+
+
+	
+	
+	
+	
+} 
+	
+
---
+++ src/main/java1/deberes/DeberFinal.java
@@ -0,0 +1,30 @@
+package deberes;
+
+public final class DeberFinal {
+
+	final private String estudianteClase = "Clase-A";
+	private String NonmbreEstudiante = " Pablo";
+	private String ApellidoEstudiante = "Perez";
+	
+
+	
+	public void horasClses() {
+		
+		System.out.println("Horario de clases 8h diarias ");
+		
+		
+	}
+	public final void recreo() {
+		
+		
+		System.out.println("1h de recreo diarias");
+		
+		
+	
+	}
+	
+	
+	
+	
+	
+}
