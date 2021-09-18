c4aa8871b5f2ed4cc9ddfeee596d82bc58566002
Roynar Sando <roynar-sandito@hotmail.com>
2021-09-17 20:17:05 -0500

    Tallerrr

---
+++ src/main/java1/ejercicio/coleccion/list/Main.java
@@ -0,0 +1,23 @@
+package ejercicio.coleccion.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// No puede contener valores primitivos, solo clases
+		
+		Integer numero1=1;
+		Integer numero2=4;
+		
+		List<Integer> listaNueva = new ArrayList<Integer>();
+		listaNueva.add(numero1);//[0]		
+		listaNueva.add(numero2);//[1]
+		
+		
+		
+	}
+
+}
